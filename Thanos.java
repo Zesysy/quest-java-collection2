34e65215b67b44a131aaffff7eee48ea6bc65d40
Sylène <sylene.manusset@gmail.com>
2020-05-15 17:59:14 +0200

    check !

--- Thanos.java
+++ Thanos.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Thanos {
@@ -13,12 +15,23 @@ public class Thanos {
 
         // TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
 
+        Collections.sort(heroes);
 
         System.out.println("\nOrder by name:");
         showList(heroes);
 
         // TODO 2: Add a Comparator and sort by age (descending)
 
+        Comparator<Hero> comparatorAgeHro = new Comparator<Hero>() {
+            @Override
+            public int compare(Hero hero1, Hero hero2) {
+                return hero2.getAge() - hero1.getAge();
+            }
+        };
+
+        Collections.sort(heroes, comparatorAgeHro);
+
+        // or Collections.sort(heroes, Comparator.comparing(Hero::getAge).reversed());
 
         System.out.println("\nOrder by age:");
         showList(heroes);
